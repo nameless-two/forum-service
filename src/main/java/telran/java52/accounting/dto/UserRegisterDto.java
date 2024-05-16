8c9d4616288454f8fe165ca78d2cdac561561d2e
jassk <jassk@host.docker.internal>
2024-05-16 20:28:01 +0400

    Added UserAccounting DTOs

---
+++ src/main/java/telran/java52/accounting/dto/UserRegisterDto.java
@@ -0,0 +1,13 @@
+package telran.java52.accounting.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserRegisterDto {
+
+	String login;
+	String password;
+	String firstName;
+	String lastName;
+
+}
---
+++ src/main/java/telran/java52/accounting/dto/UserUpdateDto.java
@@ -0,0 +1,11 @@
+package telran.java52.accounting.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserUpdateDto {
+
+	String firstName;
+	String lastName;
+	
+}
